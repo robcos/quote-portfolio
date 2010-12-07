a53fdd487ca37e8a36fb7f72415880c6ffc4b1d7
Roberto Cosenza <robcos@robcos.com>
2010-12-07 22:06:12 +0000

    Scripts

---
+++ local_download.sh
@@ -0,0 +1,2 @@
+appcfg.py download_data --application=quote-portfolio --url=http://quote-portfolio.appspot.com/_ah/remote_api --filename=data -e robcos@robcos.com
+
---
+++ upload_to_dev.sh
@@ -0,0 +1,2 @@
+appcfg.py upload_data --application=quote-portfolio --url=http://localhost:8086/_ah/remote_api --filename=data -e robcos@robcos.com
+
